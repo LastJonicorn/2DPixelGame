96e4f3768ba58e540ead07fa945f195245830415
lastjonicorn <joni.k.lampela@gmail.com>
2026-05-02 15:31:01 +0300

    Started replacing the old prompt system with a new one

--- 2DPixelGame/Assets/Scripts/HealthCollectible.cs
+++ 2DPixelGame/Assets/Scripts/HealthCollectible.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 public class HealthCollectible : MonoBehaviour
 {
     public int healthAmount = 20;
-    public Collider2D targetCollider;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision == targetCollider)
+        if (collision.CompareTag("Player"))
         {
             PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
 
